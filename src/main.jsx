6280cdd140d00d1692576c60dc334d8c19e85da0
xoqhicq1 <1508892564@qq.com>
2026-06-25 06:23:21 +0800

    Update GitHub Pages stock route

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import StockTool from './StockTool.jsx';
 import './styles.css';
 
-const CurrentApp = window.location.pathname.startsWith('/stock') ? StockTool : App;
+const CurrentApp = window.location.pathname.includes('/stock') ? StockTool : App;
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
